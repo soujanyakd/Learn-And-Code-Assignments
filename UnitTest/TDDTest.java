064f366e2c8113bb2615ef649c9848bd24820a33
Kottari Soujanya Dinesh <36099878+soujanyakd@users.noreply.github.com>
2022-11-02 15:42:52 +0530

    Update and rename UnitTestAssignment.java to TDDTest.java

--- UnitTest/TDDTest.java
+++ UnitTest/TDDTest.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class UnitTestAssignment {
+public class TDDTest {
 
     public void verifyUserProvidedNumberIsANaturalNumber(int number) {
         if(number <= 0) {
